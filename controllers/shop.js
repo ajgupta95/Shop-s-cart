b49614f3dcc9d9158936b3e4f32ce4b4f4302b82
Shivendra Jadon <shivendra@digicomm.in>
2020-02-15 00:47:35 +0530

    adding cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,6 +1,7 @@
 const Product = require('../models/product');
+const Cart = require('../models/cart');
 
-exports.getProducts = (req, res, next) => {
+exports.getProducts = (req, res, ) => {
   Product.fetchAll(products => {
     res.render('shop/product-list', {
       prods: products,
@@ -10,7 +11,17 @@ exports.getProducts = (req, res, next) => {
   });
 };
 
-exports.getIndex = (req, res, next) => {
+exports.getProudct=(req,res)=>{
+  const prodId=req.params.productId;
+  Product.findById(prodId,product=>{
+  res.render('shop/product-detail',{product:product,
+  pageTitle:product.title,
+  path:'/products'   });
+  });
+  
+};
+
+exports.getIndex = (req, res, ) => {
   Product.fetchAll(products => {
     res.render('shop/index', {
       prods: products,
@@ -20,21 +31,29 @@ exports.getIndex = (req, res, next) => {
   });
 };
 
-exports.getCart = (req, res, next) => {
+exports.getCart = (req, res, ) => {
   res.render('shop/cart', {
     path: '/cart',
     pageTitle: 'Your Cart'
   });
 };
 
-exports.getOrders = (req, res, next) => {
+exports.postCart=(req,res)=>{
+const prodId=req.body.productId;
+Product.findById(prodId,product=>{
+  Cart.addProduct(prodId,product.price);
+});
+res.redirect('/cart');
+};
+
+exports.getOrders = (req, res, ) => {
   res.render('shop/orders', {
     path: '/orders',
     pageTitle: 'Your Orders'
   });
 };
 
-exports.getCheckout = (req, res, next) => {
+exports.getCheckout = (req, res, ) => {
   res.render('shop/checkout', {
     path: '/checkout',
     pageTitle: 'Checkout'
--- models/product.js
+++ models/product.js
@@ -1,8 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const p = path.join(
-  path.dirname(process.mainModule.filename),
+const p = path.join(__dirname,'../',
   'data',
   'products.json'
 );
@@ -26,6 +25,7 @@ module.exports = class Product {
   }
 
   save() {
+    this.id=Math.random().toString();
     getProductsFromFile(products => {
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), err => {
@@ -36,5 +36,12 @@ module.exports = class Product {
 
   static fetchAll(cb) {
     getProductsFromFile(cb);
+  }
+
+  static findById(id,cb){
+    getProductsFromFile(products=>{
+      const product=products.find(p=> p.id===id);
+      cb(product); 
+    })
   }
 };
